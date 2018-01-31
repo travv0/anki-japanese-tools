05d55d6fdfbae10caf83c8ddffd5c22187907644
tssund93 <travis@sunderland.io>
2018-01-31 02:28:00 -0600

    show message on no audio found so it doesn't look inactive

--- add_audio.py
+++ add_audio.py
@@ -58,46 +58,47 @@ col.decks.current()['mid'] = model['id']
 
 deck = col.decks.byName(deckName)
 
-i = j = 0
+i = 0
 
 notes = col.findNotes('"deck:%s" %s:' % (deckName, audioFieldName))
 notecnt = len(notes)
+
+print("Attempting to add audio for %d cards..." % notecnt)
 
 for noteid in notes:
     note = col.getNote(noteid)
 
     i += 1
-    if not note.fields[audioIndex]:
-        expression = note.fields[expressionIndex]
-        reading = note.fields[readingIndex]
+    expression = note.fields[expressionIndex]
+    reading = note.fields[readingIndex]
 
-        audio = requests.get("https://assets.languagepod101.com/dictionary/japanese/audiomp3.php?",
-                             params = {'kanji': expression,
-                                       'kana': reading if reading else expression})
+    audio = requests.get("https://assets.languagepod101.com/dictionary/japanese/audiomp3.php?",
+                         params = {'kanji': expression,
+                                   'kana': reading if reading else expression})
 
-        audioFileName = 'k2a_%s_%s.mp3' % (expression, reading if reading else expression)
-        audioFilePath = '%s/%s.media/%s' % (PROFILE_HOME, collectionName, audioFileName)
-        try:
-            with open(audioFilePath, 'wb') as f:
-                f.write(audio.content)
-        except OSError as e:
-            errors.append((expression, e))
+    audioFileName = 'k2a_%s_%s.mp3' % (expression, reading if reading else expression)
+    audioFilePath = '%s/%s.media/%s' % (PROFILE_HOME, collectionName, audioFileName)
+    try:
+        with open(audioFilePath, 'wb') as f:
+            f.write(audio.content)
+    except OSError as e:
+        errors.append((expression, e))
 
-        mp3 = MP3(audioFilePath)
+    mp3 = MP3(audioFilePath)
 
-        if mp3.info.length < 5:
-            note.fields[audioIndex] = '[sound:' + audioFileName + ']'
+    if mp3.info.length < 5:
+        note.fields[audioIndex] = '[sound:' + audioFileName + ']'
 
-            print('%d/%d\tAdding audio "%s" for %s' % (i, notecnt, audioFileName, expression))
-        else:
-            os.remove(audioFilePath)
+        print('%d/%d\tAdding audio "%s" for %s' % (i, notecnt, audioFileName, expression))
+    else:
+        print('%d/%d\tAudio not found for %s, skipping...' % (i, notecnt, expression))
+        os.remove(audioFilePath)
 
-        note.flush()
+    note.flush()
 
-        j += 1
-        if j % save_interval == 0:
-            col.save()
-            print("\nCollection saved\n")
+    if i % save_interval == 0:
+        col.save()
+        print("\nCollection saved\n")
 
 print("Finished adding audio, saving collection...")
 col.close(save=True)
